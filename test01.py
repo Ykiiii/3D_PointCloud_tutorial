72fb8a0fc36b01dc34bbc682b866b199751fb202
Ykiiii <yangkaiiiiii@163.com>
2023-11-13 11:30:46 +0800

    message

--- test01.py
+++ test01.py
@@ -6,9 +6,11 @@
 #     o3d.visualization.draw(pcd)
 
 import open3d as o3d
+import cv2
+
 
 if __name__ =="__main__":
-    datadir = "J:\Document\Coding\PointCloud\cloudRgb_00000.ply"
+    datadir = "E:\program\GitCode\\3D_PointCloud\PointCloud\\rectangle.ply"
     pcd = o3d.io.read_point_cloud(datadir)
     # print(pcd)
     hull, idx = pcd.compute_convex_hull()
@@ -17,3 +19,4 @@ if __name__ =="__main__":
     hull_ls = o3d.geometry.LineSet.create_from_triangle_mesh(hull)
     hull_ls.paint_uniform_color((1,0,0))
     o3d.visualization.draw_geometries([pcd,hull_ls])
+
